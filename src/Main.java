984a3ab64158014c1b54edba43e3d86ba9c59710
. <0001155817@seinamgaluno.com.br>
2025-11-04 10:00:34 -0300

    meu primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.beans.DefaultPersistenceDelegate;
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] numeros = new int[5];
+        for (int i = 0; i <5; i++) {
+            System.out.println("Digite o "+(i+1)+" numero: ");
+            numeros[i] = sc.nextInt();
+        }
+        System.out.println("numeros digitados");
+        for (int num: numeros ){
+            System.out.println(num);
+        }
+        sc.close();
+
+
+
+    }}
